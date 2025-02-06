8af9c50dd3c6c413f9c8b2647ffe38adfc5eddf7
XimenaArmaas <ximena.armas@igrtec.com>
2025-02-06 10:41:55 -0600

    Update

--- Prueba/Controllers/HomeController.cs
+++ Prueba/Controllers/HomeController.cs
@@ -22,10 +22,6 @@ namespace Prueba.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
